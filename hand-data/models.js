d1d199ae5bbf9c87a3b42df246f3a87f0a72985b
Elizabeth Sheremet <elizabethsheremet@noutbuklizabeth.home>
2024-09-02 17:01:35 -0400

    add feature to collect all collections and generate models for these collections

--- hand-data/models.js
+++ hand-data/models.js
@@ -1,13 +1,46 @@
-const { Hook } = require("../models/hook");
-const { Reel } = require("../models/reel");
+const mongoose = require("mongoose");
+const path = require("path");
+const fs = require("fs");
 const { Rod } = require("../models/rod");
+const { Reel } = require("../models/reel");
 
+// let models = {};
+
+// function toCamelCase(str) {
+//     return str.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+// }
+
+// function capitalize(str) {
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+
+// async function initializeModels() {
+//     try {
+//         // Получаем список всех коллекций в базе данных
+//         const collections = await mongoose.connection.db.listCollections().toArray();
+
+//         collections.forEach(collection => {
+//             const collectionName = collection.name;
+//             const modelName = capitalize(toCamelCase(collectionName));
+
+//             try {
+//                 // Подключаем модель на основе имени коллекции
+//                 const model = require(path.resolve(__dirname, `../models/${modelName}`));
+//                 models[collectionName] = model[modelName];
+//             } catch (error) {
+//                 console.error(`Failed to load model for collection ${collectionName}:`, error);
+//             }
+//         });
+//     } catch (error) {
+//         console.error('Failed to initialize models:', error);
+//     }
+// }
+
+// module.exports = { models, initializeModels };
 
 const models = {
-    reels: Reel,
-    rods: Rod,
-    hooks: Hook,
+  rods: Rod,
+  reels: Reel,
 };
 
-module.exports = models;
-  
+module.exports = { models };
